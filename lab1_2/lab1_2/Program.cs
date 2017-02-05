a5cc8ddad3f22059fe1b05f9a2f5013d6771e057
Амина <@LAPTOP-ARQ6FV1L>
2017-02-06 03:51:02 +0600

    added comments

---
+++ lab1_2/lab1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_2
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+        {
+                Student1 s = new Student1("Amina", "Rakhmetalina", 1, 4);//creating new variable
+
+                Console.Write(s);
+
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab1_2/lab1_2/Student1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_2
+{
+    class Student1//creating class student
+    {
+        public string Fname;
+        public string Lname;
+        public int course;
+        public float gpa;
+
+
+        public Student1(string Fname, string Lname, int course, float gpa)//konstruktor of a class
+        {
+            this.Fname = Fname;
+            this.Lname = Lname;
+            this.course = course;
+            this.gpa = gpa;
+        }
+        public Student1(string Fname, string Lname, int course)
+        {
+            this.Fname = Fname;
+            this.Lname = Lname;
+            this.course = course;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Student\nFname:{0}\nLname: {1}\ncourse: {2}\ngpa: {3}", Fname, Lname, course, gpa);//outputing and formating
+        }
+    }
+}
