f73c4847f92b835505dbaca1a84c25e50d51b58c
aminarakhmetaly <rakhmetslinaamina@gmail.com>
2017-02-13 10:01:42 +0600

    changed some items

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -5,22 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Example1
+namespace Far_manager
 {
     class State
     {
         private int index;
 
-        public int Index 
+        public int Index
         //making the variable of index public
         {
             get { return index; }
 
-            set
-
+            set //comparing with max value
             {
-                int maxVal = Folder.GetDirectories().Length;
-                //comparing with max value
+                int maxVal = Folder.GetFileSystemInfos().Length;
                 if (value >= 0 && value < maxVal)
                 {
                     index = value;
@@ -28,58 +26,68 @@ namespace Example1
             }
         }
 
-        public DirectoryInfo Folder { get; set; }
+        public DirectoryInfo Folder;
     }
 
     class Program
     {
-        static void ShowFolderContent(State state)
-        //function that shows the content
+        static void ShowFolderContent(State state)//function that shows the content
         {
             Console.Clear();
             //cleaning the console from extra ones
 
-            DirectoryInfo[] list = state.Folder.GetDirectories(); 
+            FileSystemInfo[] list = state.Folder.GetFileSystemInfos();
             //adding an array of folders to the called "list"
-
-            for (int i = 0; i < list.Length; ++i)
+            for (int i = 0; i < list.Length; i++)
             {
-                if (state.Index == i) 
+
+                if (state.Index == i)
                 {
                     Console.BackgroundColor = ConsoleColor.White;
                     //if index equals to the ith element it changes its bg color
-                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
                 }
 
-                Console.Write(list[i]);
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-            }
 
-            foreach (FileInfo f in state.Folder.GetFiles())
-            {
-                Console.WriteLine(f.Name);
+
+                if (list[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+
+
+                Console.WriteLine(list[i].Name);
                 //using foreach outputing the names of files
             }
+
         }
 
         static void Main(string[] args)
         {
             bool alive = true;
 
-            
             State state = new State { Folder = new DirectoryInfo(@"C:\"), Index = 0 };
             //adding new variable for class of state with two items
 
-            Stack<State> layers = new Stack<State>(); 
+
+            Stack<State> layers = new Stack<State>();
             // adding the stack of states
 
-            layers.Push(state); 
+            bool modeFileOpen = false;
+
+            layers.Push(state);
             //pushing the layers into the stack
             while (alive)
             {
-                ShowFolderContent(layers.Peek());
+                if (!modeFileOpen) ShowFolderContent(layers.Peek());
                 //shows the content of initial element
 
                 ConsoleKeyInfo pressedKey = Console.ReadKey();
@@ -94,26 +102,53 @@ namespace Example1
                         layers.Peek().Index++;
                         break;
                     case ConsoleKey.Escape:
-                        layers.Pop();
+                        State top2 = layers.Peek();
+                        FileSystemInfo x2 = top2.Folder.GetFileSystemInfos()[top2.Index];
+                        //pushing the enter opens new layer
+                        if (x2.GetType() == typeof(DirectoryInfo))
+                        {
+
+                            if (layers.Count != 1)
+                            {
+
+                                layers.Pop();
+                            }
+
+
+                        }
+                        else
+                        {
+                            modeFileOpen = false;
+
+                        }
                         break;
                     case ConsoleKey.Enter:
-                       
-                        DirectoryInfo[] list = layers.Peek().Folder.GetDirectories();
-                        //pushing the enter opens new layer
-                       
-                        State newState = new State
+                        State top = layers.Peek();
+                        FileSystemInfo x = top.Folder.GetFileSystemInfos()[top.Index];
+                        if (x.GetType() == typeof(DirectoryInfo))
                         {
-                            
-                            Folder = new DirectoryInfo(list[state.Index].FullName),
-                            //naming new folder in the list
-                          
-                            Index = 0
-                        };
-                        layers.Push(newState);
-                        //adds a new state for initial ones
-                        break;
-                }
+                            State newState = new State //new class
+                            {
 
+                                Folder = x as DirectoryInfo,   //naming new folder in the list
+                                Index = 0
+                                //index at first elemement
+                            };
+                            layers.Push(newState);
+                        }
+                        else if (x.GetType() == typeof(FileInfo))
+                        {
+                            FileStream sr = new FileStream(x.FullName, FileMode.Open, FileAccess.Read);//opening and reading data
+                            StreamReader g = new StreamReader(sr);
+                            string s = g.ReadToEnd();
+                            Console.Clear();
+                            Console.Write(s);
+                            modeFileOpen = true;//opens file
+                        }
+                        break;
+
+
+                }
             }
         }
     }
