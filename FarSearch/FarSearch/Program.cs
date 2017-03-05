525e05ca83ef591ed129e3adbef7c7f4db7e11b8
Амина <@LAPTOP-ARQ6FV1L>
2017-03-05 20:16:28 +0600

    Izi

---
+++ FarSearch/FarSearch/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAR_MNGR
+{
+    class State
+    {
+        private int index;
+        public int Index
+        {
+            get { return index; }
+            set
+            {
+                int maxVal = Folder.GetFileSystemInfos().Length;
+                if (value >= 0 && value < maxVal)
+                {
+                    index = value;
+                }
+            }
+        }
+        public DirectoryInfo Folder { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setting default color for manager
+
+            Console.WriteLine(@"Че хочешь?");
+            String Q = Console.ReadLine();
+            Stack<DirectoryInfo> layers = new Stack<DirectoryInfo>();
+            layers.Push(new DirectoryInfo(@"C:\Users\Амина\Desktop\уроки"));
+            while (layers.Count > 0) {
+                DirectoryInfo CurState = layers.Peek();
+                layers.Pop();
+                for (int i = 0; i < CurState.GetFileSystemInfos().Length; i++) {
+                    if (CurState.GetFileSystemInfos()[i] is DirectoryInfo)
+                    {
+                        layers.Push(new DirectoryInfo(CurState.GetFileSystemInfos()[i].FullName));
+                    }  else {
+                        if (CurState.GetFileSystemInfos()[i].Extension == ".txt") {
+                            if (DoesItContain(CurState.GetFileSystemInfos()[i].FullName, Q)) {
+                                Console.WriteLine(CurState.GetFileSystemInfos()[i].FullName);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+
+        }
+        static bool DoesItContain(string fullName, string query) {
+
+            StreamReader tmp = new StreamReader(new FileStream(fullName, FileMode.Open, FileAccess.Read));
+            string s = tmp.ReadToEnd();
+            tmp.Close();
+            return s.Contains(query);
+        }
+        
+
+    }
+}
